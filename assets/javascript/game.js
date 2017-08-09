03fc7de26765ebffb9fec2a6db5961d229c175dd
Michael Nehas <Michael Nehas>
2017-08-08 21:23:36 -0400

    week-4-game files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,44 +2,50 @@ $(document).ready(function() {
 
     var losses = 0;
     var wins = 0;
-    var userTotal = 0;
+    var redCrystal = Math.floor(Math.random()*12)+1;
+    var blueCrystal = Math.floor(Math.random()*12)+1;
+    var greenCrystal = Math.floor(Math.random()*12)+1;
+    var yellowCrystal = Math.floor(Math.random()*12)+1;
+    var randomNumber = Math.floor(Math.random()*100)+1;
+    var totalScore = 0;
+
 
 
     $("#red").on("click", function() {
-
-
-        var random = Math.floor(Math.random() * 100) + 1;
-
-        $("#random").html(random);
+        totalScore += redCrystal;
+        $("#random").html(randomNumber);
+        $("#random2").html(redCrystal);
+        $("#random2").html(totalScore);
     });
+
 
     $("#blue").on("click", function() {
-
-
-        var random = Math.floor(Math.random() * 100) + 1;
-
-        $("#random").html(random);
-
+        totalScore += blueCrystal;
+        $("#random").html(randomNumber);
+        $("#random2").html(blueCrystal);
+        $("#random2").html(totalScore);
     });
+
+
+
+    $("#yellow").on("click", function() {
+        totalScore += yellowCrystal;
+        $("#random").html(randomNumber);
+        $("#random2").html(yellowCrystal);
+        $("#random2").html(totalScore);
+    });
+
 
 
     $("#green").on("click", function() {
-
-
-        var random = Math.floor(Math.random() * 100) + 1;
-
-        $("#random").html(random);
-
+        totalScore += greenCrystal;
+        $("#random").html(randomNumber);
+        $("#random2").html(greenCrystal);
+        $("#random2").html(totalScore);
     });
 
-    $("#yellow").on("click", function() {
 
-
-        var random = Math.floor(Math.random() * 100) + 1;
-
-        $("#random").html(random);
-
-    });
+  
 
 
 
